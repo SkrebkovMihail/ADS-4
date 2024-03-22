f7c04724d685bc878539c551ce2445d08bf38685
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-03-22 12:47:39 +0300

    Skrebkov_Mihail

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,49 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                if (arr[i] != arr[j] || (i > 0 && arr[i] != arr[i-1])) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int i = 0, j = len - 1;
+    while (i < j) {
+        if (arr[i] + arr[j] == value) {
+            if (arr[i] != arr[j] || (i > 0 && arr[i] != arr[i-1])) {
+                count++;
+            }
+            i++;
+            j--;
+        } else if (arr[i] + arr[j] < value) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int *pos = std::lower_bound(arr + i + 1, arr + len, target);
+        if (pos != arr + len && *pos == target) {
+            if (i == 0 || arr[i] != arr[i-1] || (i > 0 && arr[i] != arr[i-1])) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
