ed50f0f8f552791b10fbb1c6b0f23a2daaf6546c
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-03-22 13:09:33 +0300

    Skrebkov_Mihail

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,37 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+using namespace std;
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
-                break;
+                while (j < len - 1 && arr[j] == arr[j + 1]) {
+                    j++;
+                }
             }
         }
     }
     return count;
 }
 
-
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0, right = len - 1;
     while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
+        if (arr[left] + arr[right] == value) {
             count++;
+            while (left < right && arr[left] == arr[left + 1]) {
+                left++;
+            }
+            while (left < right && arr[right] == arr[right - 1]) {
+                right--;
+            }
             left++;
             right--;
-        } else if (sum < value) {
+        } else if (arr[left] + arr[right] < value) {
             left++;
         } else {
             right--;
@@ -31,20 +40,15 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 
-
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int i = 0, j = len - 1;
-    while (i < j) {
-        if (arr[i] + arr[j] == value) {
-            count++;
-            i++;
-            j--;
-        } else if (arr[i] + arr[j] < value) {
-            i++;
-        } else {
-            j--;
+    int *countArr = new int[value + 1]();
+    for (int i = 0; i < len; i++) {
+        if (value - arr[i] >= 0 && countArr[value - arr[i]] > 0) {
+            count += countArr[value - arr[i]];
         }
+        countArr[arr[i]]++;
     }
+    delete[] countArr;
     return count;
 }
